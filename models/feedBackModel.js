adbee30672a52739676160b96d012a5df7a26eb8
User-Err <prospermadojmeu00@gmail.com>
2024-09-15 19:18:39 +0100

    IDK COMMIT PUSH

--- models/feedBackModel.js
+++ models/feedBackModel.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
 const feedBackModel = new mongoose.Schema({
-    name: {type: String, }
-})
+    name: {type: String, required: true},
+    phone: {type: String, required: true},
+    content: {type: String, required: true},
+    isRead : {type:Boolean,default: false}
+});

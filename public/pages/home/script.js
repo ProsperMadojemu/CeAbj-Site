454abc9956c3321647fe41d04c4fbfdf004293ad
User-Err <prospermadojmeu00@gmail.com>
2024-12-17 22:10:55 +0100

    DRAWER

--- public/pages/home/script.js
+++ public/pages/home/script.js
@@ -243,7 +243,19 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.error("Element(s) not found: #scroll-video or #video-container");
     }
-    
+    // DOM Elements
+    const menuBtn = document.getElementById("menu-btn");
+    const closeBtn = document.getElementById("close-btn");
+    const drawer = document.getElementById("drawer");
+
+    menuBtn.addEventListener("click", () => {
+    drawer.classList.add("open");
+    });
+
+    closeBtn.addEventListener("click", () => {
+    drawer.classList.remove("open");
+    });
+
 
     // if (video && videoContainer) {
     //     gsap.registerPlugin(ScrollTrigger);
